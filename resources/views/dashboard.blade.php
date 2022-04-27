7a870f1fe2abf2f8cb4dda932914c4f3ca8a0187
MigArv96 <84158783+MigArv96@users.noreply.github.com>
2022-04-27 16:04:08 -0600

    Ultimo commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Jose Miguel Arevalo Moreira</th>
+      <th scope="col">2016AM606</th>
+
+    </tr>
+    <tr>
+      <th scope="col">Tecnologias Emergentes</th>
+      <th scope="col">Proyecto CRUD</th>
+      
+
+    </tr>
+  </thead>
+</table>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductosController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', function () 
+{
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+
+Route::get('/productos', [ProductosController::class, 'index'])->name('productos');
+Route::get('/productos/create', [ProductosController::class, 'create'])->name('create');
+Route::post('create', [ProductosController::class, 'store'])->name('store');
+Route::delete('/productos/{id}', [ProductosController::class, 'destroy'])->name('destroy');
+//Route::get('/productos/{id}/edit', [ProductosController::class, 'edit'])->name('edit');
+Route::put('/productos/update', [ProductosController::class, 'update'])->name('update');
 
 require __DIR__.'/auth.php';
